e47eb420c5e75574234e4084107db1ca66a5db34
grothej <justinmaxgrothe@gmail.com>
2025-06-28 17:18:52 +0200

    doc: update command describtion

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,5 @@
 /*
-Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
+Copyright © 2025 Justin Grothe
 */
 package cmd
 
@@ -10,21 +9,14 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "go-clean-up.git",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Use:   "go-clean-up",
+	Short: "Cli Tool for cleaning unused files on your system",
+	Long: ` "go-clean-up" helps with cleaning old, unused and redundant files from your system
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+ usage: go-clean-up [] `,
+	Run: func(cmd *cobra.Command, args []string) {},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -47,5 +39,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
