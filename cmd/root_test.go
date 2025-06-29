58eb990d9d604d188a6e9679c82314a0a4b2d8cc
grothej <justinmaxgrothe@gmail.com>
2025-06-29 21:57:07 +0200

    feat: add test for cleaning

---
+++ cmd/root_test.go
@@ -0,0 +1,53 @@
+package cmd
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func setupTestDir(fileNames []string, t *testing.T) {
+	dir, err := os.MkdirTemp("", "test-dir")
+	if err != nil {
+		t.Fatalf("Failed to create dir: '%s'\nerr: %v", dir, err)
+	}
+
+	for _, file := range fileNames {
+		path := filepath.Join(dir, file)
+		_, err = os.Create(path)
+		if err != nil {
+			t.Fatalf("Failed to create file: '%s'\nerr: %v", dir, err)
+		}
+	}
+
+	err = os.Chdir(dir)
+	if err != nil {
+		t.Fatalf("Failed to change to directory: '%s'\nerr: %v", dir, err)
+	}
+
+	t.Cleanup(func() {
+		err = os.RemoveAll(dir)
+		if err != nil {
+			t.Fatalf("Couldn't remove dir and files: %v", err)
+		}
+	})
+}
+
+func assertFilesAreDeleted(filenames []string, t *testing.T) {
+	for _, pattern := range filenames {
+		wd, _ := os.Getwd()
+		path := filepath.Join(wd, pattern)
+		if matches, _ := filepath.Glob(path); len(matches) > 0 {
+			t.Errorf("Expected '%s' to be deleted but was still found", pattern)
+		}
+	}
+}
+
+func TestDefaultClean(t *testing.T) {
+	filesToDelete := []string{"old-log.log", ".DS_Store"}
+	setupTestDir(filesToDelete, t)
+
+	clean()
+
+	assertFilesAreDeleted(filesToDelete, t)
+}
