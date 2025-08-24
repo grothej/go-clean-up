f078ec4c9ee8d8faf3ee81955001394da837067e
grothej <justinmaxgrothe@gmail.com>
2025-08-25 00:07:23 +0200

    Fix: Remove debug code

--- cmd/clean/fileAge.go
+++ cmd/clean/fileAge.go
@@ -19,8 +19,5 @@ var fileTTL = timeOffset{
 
 func IsFileOlderThanTTL(info fs.FileInfo) bool {
 	modTimeThresholdDate := info.ModTime().AddDate(fileTTL.years, fileTTL.months, fileTTL.days)
-	if modTimeThresholdDate.After(time.Now()) {
-		time.Sleep(10000)
-	}
 	return modTimeThresholdDate.Before(time.Now())
 }
